f01b6403328b1533819ca69bec0364e008ac2eb4
Yarden Lawson <yardenlawson@gmail.com>
2021-04-11 19:40:39 +0100

    Icons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-// import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Container from '@material-ui/core/Container';
+// import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Create() {
   return (
@@ -22,13 +23,24 @@ export default function Create() {
         type="submit"
         color="secondary"
         variant="contained"
+        // startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
       >
         Submit
       </Button>
+
+
     </Container>
   );
 }
 
+
+
+
+
+
+//Buttons
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
 {
   /* <Button type="submit">Submit</Button>
       <Button type="submit" color="secondary" variant="outlined">
@@ -41,3 +53,15 @@ export default function Create() {
         <Button type="submit">THREE</Button>
       </ButtonGroup> */
 }
+
+
+
+{/* ICONS
+  import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+      <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large"/>
+      <AcUnitOutlinedIcon color="secondary" fontSize="small"/>
+      <AcUnitOutlinedIcon color="action" fontSize="small"/>
+      <AcUnitOutlinedIcon color="error" fontSize="small"/>
+      <AcUnitOutlinedIcon color="disabled" fontSize="small"/> */}
