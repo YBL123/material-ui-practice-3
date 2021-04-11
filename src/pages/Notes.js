06d8502b81d0c02dc4dad1d48d7b7977bbf889ab
Yarden Lawson <yardenlawson@gmail.com>
2021-04-11 22:17:37 +0100

    Card Components

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
+import NoteCard from '../components/NotesCard';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -13,18 +14,33 @@ export default function Notes() {
       .then((data) => setNotes(data));
   }, []);
 
+  //delete from api + our State
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:8000/notes/' + id, {
+      method: 'DELETE',
+    });
+    const newNotes = notes.filter(note => note.id !== id)
+    setNotes(newNotes);
+  };
+
   return (
     <Container>
-      <Grid container>
+      <Grid container spacing={3}>
         {notes.map((note) => (
           <Grid item key={note.id} xs={12} sm={6} lg={4}>
-            <Paper>{note.title}</Paper>
+            <NoteCard note={note} handleDelete={handleDelete}/>
           </Grid>
         ))}
       </Grid>
     </Container>
   );
 }
+
+
+
+
+
+
 
 //GRID!!!
 {
