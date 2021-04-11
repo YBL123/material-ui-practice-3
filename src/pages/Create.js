d3f0c74075dc2aa164c14a9aaf22f687cb9e0e59
Yarden Lawson <yardenlawson@gmail.com>
2021-04-11 19:22:33 +0100

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        //sets it to an h2 rather than an h6 -> although it will still look the same
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create A New Note
+      </Typography>
     </div>
+    
   )
 }
